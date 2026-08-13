a64cb0428caef07590ddbb8bbd203ca046a1cd90
Md Iqbal Hossen <iqbal@Mds-Mac-mini.local>
2026-08-13 20:26:32 +0600

    style: add top padding to main content area in admin layout

--- src/app/system-hq/(admin)/layout.tsx
+++ src/app/system-hq/(admin)/layout.tsx
@@ -22,7 +22,7 @@ export default function AdminLayout({
       <div className="flex-1 md:ml-64 flex flex-col pb-20 md:pb-0">
         <AdminHeader />
         
-        <main className="flex-1 px-4 md:px-8 pb-8">
+        <main className="flex-1 px-4 md:px-8 pt-8 pb-8">
           {children}
         </main>
         
